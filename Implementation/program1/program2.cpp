02c945767c18d2cfe9810e801fa94dba473b6b4a
beebelucas <beebelucas@gmail.com>
2023-04-18 21:01:41 -0500

    Implementation complete. Next is adding comments and rules.

--- Implementation/program1/program2.cpp
+++ Implementation/program1/program2.cpp
@@ -9,11 +9,75 @@
  */
 
 #include <string>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-string getInputString();
+char findCommonChar(string givenString){
+    string sortedString = givenString;
+    sort(sortedString.begin(), sortedString.end());
+    char commonChar;
+    int totalNum = 0;
+    int currentNum = 0;
+    for(int i = 0; i < sortedString.length(); i++){
+        if(i == 0){
+            currentNum = 1;
+        }else if(sortedString[i] == sortedString[i-1]){
+            currentNum++;
+        }else{
+            currentNum = 1;
+        }
+
+        if(currentNum > totalNum){
+            totalNum = currentNum;
+            commonChar = sortedString[i];
+        }
+    }
+    return commonChar;
+
+}
+
+bool isPalindrome(string givenString){
+    string compareStr = givenString;
+    int j = 0;
+    
+    for(int i = givenString.length()-1; i >= 0; i--){
+        compareStr[j] = givenString[i];
+        j++;
+    }
+    
+    bool rtnVal = true;
+    for(int i = 0; i < givenString.length(); i++){
+        if(givenString[i] != compareStr[i]){
+            rtnVal = false;
+        }
+    }
+    return rtnVal;
+}
 
 int main(){
     string inputVal;
+
+    cout << "Enter a string (enter '0' to quit): ";
+    cin >> inputVal;
+
+    while(inputVal.compare("0") != 0){
+        
+        cout << "\nAnalysis: \n";
+        cout << "Length: " << inputVal.length() << "\n";
+        cout << "Most common char: " <<findCommonChar(inputVal) << "\n";
+
+        
+        if(isPalindrome(inputVal)){
+            cout << "Is Palindrome: Yes\n";
+        }else{
+            cout << "Is Palindrome: No\n";
+        }
+        
+        cout << "\n";
+        cout << "Enter a string (enter '0' to quit): ";
+        cin >> inputVal;
+    }
+
 }
