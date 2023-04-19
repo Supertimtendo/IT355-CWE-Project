a54f4a834a0f3f248592aab0b46b6b5e709fa23b
beebelucas <beebelucas@gmail.com>
2023-04-18 20:13:30 -0500

    Came up with program idea. Starting Implementation

--- Implementation/program1/program2.cpp
+++ Implementation/program1/program2.cpp
@@ -1,7 +1,19 @@
 /**
- * 
+ * Program to run analysis on user input data. It tells the user whether
+ * a given input is a palindrome, how many characters are used, and which 
+ * character appears most.
  * 
  * By Lucas Beebe
  * 
  * 
  */
+
+#include <string>
+
+using namespace std;
+
+string getInputString();
+
+int main(){
+    string inputVal;
+}
